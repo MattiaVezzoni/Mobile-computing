ef70b2e8367adb114cc8b7fe77a689ce35fc8eb2
MattiaVezzoni <mattiavezzoni@ucwv.edu>
2023-01-21 16:06:38 -0500

    commit homework4 week2

---
+++ Homeworw_class/Homework4_week2.kt
@@ -0,0 +1,9 @@
+package Homeworw_class
+
+fun main() {
+    val iOS: Boolean = "I have an iOS device."
+    val android: Boolean = "I have an Android device."
+
+    println(iOS)
+    println(android)
+}
