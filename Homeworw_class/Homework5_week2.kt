06ae02f5dfae019de469a7e06b18ee3fa2299f60
MattiaVezzoni <mattiavezzoni@ucwv.edu>
2023-01-21 16:31:10 -0500

    commit homework5 week2

---
+++ Homeworw_class/Homework5_week2.kt
@@ -0,0 +1,5 @@
+package Homeworw_class
+
+fun main(){
+    println("Hi! My name is $firstname $lastname and I am $age, I currently study at University of Charleston, and $false. Today is $5 and I enjoy $hob1 and $hob3")
+}
