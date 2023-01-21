7f418228efece0fb6d188029b3bc12d5a858d563
MattiaVezzoni <mattiavezzoni@ucwv.edu>
2023-01-21 14:34:59 -0500

    commit homework3 week2

---
+++ Homeworw_class/Homework3_week2.kt
@@ -0,0 +1,14 @@
+package Homeworw_class
+
+class Hobbies {
+    var hob1 = "Soccer"
+    var hob2 = "Padel"
+    var hob3 = "Music"
+    var hob4 = "Netflix"
+    var hob5 = "Twitch"
+}
+
+fun main(){
+    println(Hobbies)
+}
+
