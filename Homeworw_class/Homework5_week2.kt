23a2ae8bb8b129dfe58b7047381b2405e660cfe0
MattiaVezzoni <90865450+MattiaVezzoni@users.noreply.github.com>
2023-02-03 12:28:00 -0500

    Update Homework5_week2.kt

--- Homeworw_class/Homework5_week2.kt
+++ Homeworw_class/Homework5_week2.kt
@@ -1,5 +1,5 @@
 package Homeworw_class
 
 fun main(){
-    println("Hi! My name is $firstname $lastname and I am $age, I currently study at University of Charleston, and $false. Today is $5 and I enjoy $hob1 and $hob3")
+    println("Hi! My name is $firstname $lastname and I am $age, I currently study at University of Charleston, and $false. Today is $5 and I enjoy $myArray[1] and $myArray[3]")
 }
